c69621b355a66158a0f0d5447c77b0ec9174b267
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-12-09 11:41:57 +0530

    Update 10fork to use two file descriptor for communication

--- Process/fork/10fork.c
+++ Process/fork/10fork.c
@@ -9,8 +9,12 @@
 #include <unistd.h>
 
 int main(int argc, char const *argv[]) {
-  int p1[2];
+  int p1[2];  // C => P
+  int p2[2];  // P => C
   if (pipe(p1) == -1) {
+    return 1;
+  }
+  if (pipe(p2) == -1) {
     return 1;
   }
   int pid = fork();
@@ -18,8 +22,11 @@ int main(int argc, char const *argv[]) {
     return 2;
   }
   if (pid == 0) {
+    // Child Process
+    close(p1[0]);
+    close(p2[1]);
     int x;
-    if (read(p1[0], &x, sizeof(x)) == -1) {
+    if (read(p2[0], &x, sizeof(x)) == -1) {
       return 3;
     }
     printf("Received from parent : %d\n", x);
@@ -27,21 +34,26 @@ int main(int argc, char const *argv[]) {
     if (write(p1[1], &x, sizeof(x)) == -1) {
       return 4;
     }
-    printf("Wrote to parent : %d\n", x);
+    // printf("Wrote to parent : %d\n", x);
+    close(p1[1]);
+    close(p2[0]);
   } else {
+    // Parent Process
+    close(p2[0]);
+    close(p1[1]);
     srand(time(NULL));
     int y = rand() % 10;
-    if (write(p1[1], &y, sizeof(y)) == -1) {
+    if (write(p2[1], &y, sizeof(y)) == -1) {
       return 5;
     }
-    printf("Wrote to child : %d\n", y);
+    // printf("Wrote to child : %d\n", y);
     if (read(p1[0], &y, sizeof(y)) == -1) {
       return 6;
     }
     printf("Received from child: %d\n", y);
+    close(p1[0]);
+    close(p2[1]);
     wait(NULL);
   }
-  close(p1[0]);
-  close(p1[1]);
   return 0;
 }
